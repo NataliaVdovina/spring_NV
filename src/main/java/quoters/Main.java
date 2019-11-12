5683ecff7a519efc15a86614fffe8a2ea7621c8b
Natalia Vdovina <nat12-09@mail.ru>
2019-11-12 11:19:39 +0300

    3-phased constructor

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -4,10 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException{
-        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("context.xml");
-        while (true) {
-            Thread.sleep(100);
-            classPathXmlApplicationContext.getBean(Quoter.class).sayQuote();
-        }
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        context.getBean(Quoter.class).sayQuote();
     }
 }
